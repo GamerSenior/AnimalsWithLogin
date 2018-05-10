8a1e91a97f85c7ae3e8fd8f5b1d10e7469ca54ee
Giovani Garcia Abel <abelgiovani@gmail.com>
2018-05-10 11:38:53 -0300

    Initial commit

---
+++ app/src/main/java/com/blue/giovani/login/AnimaisActivity.java
@@ -0,0 +1,13 @@
+package com.blue.giovani.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnimaisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animais);
+    }
+}
